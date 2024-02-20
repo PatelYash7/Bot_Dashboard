b11bb5f53d5f3a4471161b6ef5d4ec4763f64386
PatelYash7 <yashpate0101@gmail.com>
2024-02-20 18:41:49 +0530

    Navbar Added

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,7 +1,33 @@
-function Navbar():JSX.Element {
+import ProfileBox from "./ProfileBox";
+import image from "../Resources/images/TechTOnions Logo Compact 13.png"
+import Userprofile from "./Userprofile";
+
+
+function Navbar(): JSX.Element {
+  const img: string =
+    "https://images.pexels.com/photos/325521/pexels-photo-325521.jpeg";
+
   return (
-    <div>Navbar</div>
-  )
+      <div className="flex justify-between gap-10 items-center min-w-full">
+        <div className="flex items-center gap-10">
+          {/* Logo and Name */}
+          <div className="flex items-center">
+            <img src={image}  className="  h-10 w-10 rounded-2xl"alt="" />
+            <p className="text-white">TechTOnion</p>
+          </div>
+          <div className=" bg-lightbg rounded-3xl">
+            <ProfileBox name="Near Hack's" img={img} />
+          </div>
+        </div>
+        <div className="flex items-center ">
+          <button onClick={(): void => {
+            console.log("Clicked");
+          }} className="">
+          <Userprofile name="Yash" img={img} />
+          </button>
+        </div>
+      </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,16 +1,24 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar";
+import Dropdown from "../Components/Dropdown";
 
 function Home(): JSX.Element {
   return (
-    <div className="flex flex-col">
-      <div className="bg-blue-300 h-16 w-full">
+    <div className="flex flex-col relative max-h-screen ">
+      {/* Navbar */}
+      <div className="h-14 bg-navColor flex items-center px-8 py-4 sticky top-0"> 
         <Navbar/>
       </div>
+      {/* Main Content */}
       <div className=" flex justify-between ">
-        <div className="h-screen bg-green-300 w-[30%] font-main font-medium text-2xl">Hello Home</div>
-        <div className="bg-red-300 h-screen w-[70%]">
+        {/* Sidebar Content */}
+        <div className="h-screen bg-sideColor w-[20%] font-main font-medium text-2xl pl-8">
+          Hello Home
+        </div>
+        {/* Display Components */}
+        <div className="bg-red-300 h-screen w-[80%] pr-8">
           <Outlet />
+          <Dropdown/>
         </div>
       </div>
     </div>
