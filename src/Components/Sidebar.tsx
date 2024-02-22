a0963a435ebbe87e1498dbadff55db237b09d1d6
PatelYash7 <yashpate0101@gmail.com>
2024-02-22 13:29:57 +0530

    Added Sidebar content

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,16 +1,12 @@
 import Subsidebar from "./Subsidebar"
-import Bar from "../Resources/images/Bar.png"
+import {DataSidebar} from "../Resources/Data"
 
 function Sidebar() {
   return (
-    <div className="flex flex-col gap-3 items-center mr-2 ">
-            <Subsidebar name="Settings" img={Bar}/>        
-            <Subsidebar name="Settings" img={Bar}/>
-            <Subsidebar name="Settings" img={Bar}/>
-            <Subsidebar name="Settings" img={Bar}/>
-            <Subsidebar name="Settings" img={Bar}/>
-            <Subsidebar name="Settings" img={Bar}/>
-            <Subsidebar name="Settings" img={Bar}/>
+    <div className="flex flex-col  items-start justify-center min-w-full mr-1 ">
+      <div  className="">
+            {DataSidebar.map((item,key)=><Subsidebar key={key} name={item.name} route={item.route} img={item.img}/>)}
+      </div>
     </div>
   )
 }
--- src/Components/SidebarHeader.tsx
+++ src/Components/SidebarHeader.tsx
@@ -4,11 +4,11 @@ import Reload from "../Resources/images/Reload.png";
 
 function SidebarHeader(): JSX.Element {
   return (
-    <div className="flex justify-center gap-2 text-white items-center    m-4 ">
+    <div className="flex justify-center gap-2 text-white items-center    m-2 ">
       <Link to={'/home'}>
-        <div className="flex justify-center gap-2 bg-sidebarHead rounded-2xl w-full px-3  py-1 ">
-          <img src={Home} alt="svg" className="h-5 w-5" />
-          <div>Home</div>
+        <div className="flex justify-center items-center gap-2 bg-sidebarHead rounded-2xl w-full px-3 py-1 cursor-pointer hover:transition transition hover:bg-hoverbg">
+          <img src={Home} alt="svg" className="h-4 w-4" />
+          <div className="text-sm">Home</div>
         </div>
       </Link>
       <div className="bg-lightbg w-[30%] py-1 px-2 flex justify-center rounded-xl hover:transition transition hover:bg-hoverbg">
