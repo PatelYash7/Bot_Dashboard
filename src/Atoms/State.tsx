5a81090a1bd320479c9fc70e147a3cfce2e56b3c
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2024-03-01 20:00:34 +0530

    Moderation Page Finally Done

--- src/Atoms/State.tsx
+++ src/Atoms/State.tsx
@@ -1,11 +1,10 @@
-import { atom } from "recoil";
+import { atom} from "recoil";
 
 export const toggleState = atom({
     key: "toggleState",
     default: false,
 })
-
-export const moderationToggleState = atom({
-    key:"moderationToggleState",
-    default:false
+export const ModerationPath = atom({
+    key:"ModerationPath",
+    default:" "
 })
--- src/Pages/Moderation.tsx
+++ src/Pages/Moderation.tsx
@@ -1,52 +1,52 @@
-import { useRecoilState } from "recoil";
 import Heading from "../Components/Heading";
 import { Link, Outlet, useLocation } from "react-router-dom";
-import { moderationToggleState } from "../Atoms/State";
+
+import Expand from "../Components/Expand";
+import { useRecoilState } from "recoil";
+import { ModerationPath } from "../Atoms/State";
+
 
 function Moderation(): JSX.Element {
-  const[view,Setview]=useRecoilState(moderationToggleState)
+  
   const location = useLocation();
   const { pathname } = location;
   const DesiredRoute = "/moderation/message-type";
-  console.log(view)
+  const LevelRoute = "/moderation/level-setup";
+  const [modePathValue,setModePathValue] = useRecoilState(ModerationPath);
+  setModePathValue(pathname);
+  if(modePathValue === DesiredRoute){
+    return (
+        <div className="mt-10 ml-8">
+            <div className="my-4 flex justify-start gap-4 items-center">
+              <Link to={"/moderation"} >
+                <Heading head="Moderation" />
+              </Link>
+              <Expand routeName="Message Type" />
+            </div>
+            <Outlet />
+        </div>
+    )
+  }  
+  if (modePathValue === LevelRoute){
+    return(
+    <div className="mt-10 ml-8">
+            <div className="my-4 flex justify-start gap-4 items-center">
+              <Link to={"/moderation"} >
+                <Heading head="Moderation" />
+              </Link>
+              <Expand routeName="Level Setup" />
+            </div>
+            <Outlet />
+        </div>
+    )  
+  }
+  
   return (
     <div className="mt-10 ml-8">
-      {
-      view ? (
-        <div className="my-4 flex justify-start gap-4 items-center">
-          <Link to={"/moderation"} onClick={():void=>{Setview(prev => !prev)}} >
-            <Heading head="Moderation" />
-          </Link>
-              <div className="text-justify mt-3">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="white"
-                  className="w-8 h-8"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m8.25 4.5 7.5 7.5-7.5 7.5"
-                  />
-                </svg>
-              </div>
-          <div className="text-white cursor-default text-3xl mt-2 font-medium">
-            Message Type
-          </div>
-        </div>
-      ) : (
-        <Link to={"/moderation"} >
-          <Heading head="Moderation" />
-        </Link>
-      )}
-
-      {DesiredRoute === pathname ? (
-        <Outlet />
-      ) : (
-        <Link to="/moderation/message-type" onClick={():void=>{Setview(true)}}>
+      <div className="my-4 flex justify-start gap-4 items-center">
+                <Heading head="Moderation" />
+      </div>
+      <Link to="/moderation/message-type">
           <div className="w-full bg-lightbg hover:bg-hoverbg transition-all rounded-lg mt-4 py-3 px-3 flex justify-start gap-3 items-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -65,7 +65,25 @@ function Moderation(): JSX.Element {
             </div>
           </div>
         </Link>
-      )}
+        <Link to="/moderation/level-setup" >
+          <div className="w-full bg-lightbg hover:bg-hoverbg transition-all rounded-lg mt-4 py-3 px-3 flex justify-start gap-3 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="white"
+              className="w-6 h-6"
+            >
+              <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+              <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+            </svg>
+            <div className="text-white px-3 ">
+              <p className="text-xl font-medium">Level Setup</p>
+              <p className="text-lg">
+                Setup the levels for the User and Manage their Roles on the basis of their Levels
+              </p>
+            </div>
+          </div>
+        </Link>
     </div>
   );
 }
